6af697892743ab5f48b0b8ddd0a051685468204b
Светлана Барашкова <svetabarash04@mail.ru>
2024-11-02 21:11:16 +0700

    валидация

--- lab4.py
+++ lab4.py
@@ -86,7 +86,7 @@ def power():
 
 tree_count = 0
 
-@lab4.route('/lab4/tree', methods= ['GET', 'POST'])
+@lab4.route('/lab4/tree', methods=['GET', 'POST'])
 def tree():
     global tree_count
     if request.method == 'GET':
@@ -94,9 +94,10 @@ def tree():
     
     operation = request.form.get('operation')
 
-    if operation == 'cut':
+    # Обработка операций с проверкой на границы
+    if operation == 'cut' and tree_count > 0:
         tree_count -= 1
-    elif operation == 'plant':
+    elif operation == 'plant' and tree_count < 10:
         tree_count += 1
 
     return redirect('/lab4/tree')
