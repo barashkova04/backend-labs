dc04ae99f8b8532601ab9ac601e985ed3c22bcbc
Светлана Барашкова <svetabarash04@mail.ru>
2024-12-11 13:42:55 +0700

    праавки

--- lab6.py
+++ lab6.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session
-lab4 = Blueprint('lab6', __name__)
+lab6 = Blueprint('lab6', __name__)
 
 
-@lab4.route('/lab6/')
+@lab6.route('/lab6/')
 def lab():
     return render_template('lab6/lab6.html')
