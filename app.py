ee2083d6013775f56165f5523a674f900c5bf81c
Светлана Барашкова <svetabarash04@mail.ru>
2024-12-21 19:49:02 +0700

    заготовка к 7 лаб

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 
 app = Flask(__name__)
 
@@ -18,6 +19,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -138,6 +140,9 @@ def start():
                 </li>
                 <li>
                     <a href="/lab6">Лабораторная работа 6</a>
+                </li>
+                <li>
+                    <a href="/lab7">Лабораторная работа 7</a>
                 </li>
 
             </ol>
