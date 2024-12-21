a06ca6749a5bfb770881551b518020674a595115
Светлана Барашкова <svetabarash04@mail.ru>
2024-12-21 22:00:18 +0700

    вывод таблицы

---
+++ static/lab7/main.js
@@ -0,0 +1,53 @@
+function fillFilmList() {
+    fetch('/lab7/rest-api/films/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (films) {
+        let tbody = document.getElementById('film-list');
+        tbody.innerHTML = '';
+        for(let i = 0; i < films.length; i++) { // Исправлено на films.length
+            let tr = document.createElement('tr');
+
+            let tdTitle = document.createElement('td');
+            let tdTitleRus = document.createElement('td');
+            let tdTitleYear = document.createElement('td');
+            let tdActions = document.createElement('td');
+
+            tdTitle.innerText = films[i].title === films[i].title_ru ? '' : films[i].title;
+            tdTitleRus.innerText = films[i].title_ru;
+            tdTitleYear.innerText = films[i].year;
+
+            let editButton = document.createElement('button');
+            editButton.innerText = 'редактировать';
+            editButton.onclick = function() {
+                // Здесь можно добавить логику редактирования
+                alert('Редактирование фильма: ' + films[i].title);
+            };
+
+            let delButton = document.createElement('button');
+            delButton.innerText = 'удалить';
+            delButton.onclick = function() {
+                // Здесь можно добавить логику удаления
+                fetch('/lab7/rest-api/films/' + i, {
+                    method: 'DELETE'
+                }).then(() => {
+                    fillFilmList(); // Обновляем список после удаления
+                });
+            };
+
+            tdActions.appendChild(editButton);
+            tdActions.appendChild(delButton);
+
+            tr.appendChild(tdTitle);
+            tr.appendChild(tdTitleRus);
+            tr.appendChild(tdTitleYear);
+            tr.appendChild(tdActions);
+
+            tbody.appendChild(tr);
+        }
+    })
+    .catch(function (error) {
+        console.error('Ошибка:', error);
+    });
+}
