2da745f994d39ecb2e0b810cfb6a0512ea531d3f
Светлана Барашкова <svetabarash04@mail.ru>
2024-12-26 10:13:14 +0700

    правки

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
 from flask import Flask, url_for
+from os import path
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from db.models import users
+from flask_login import LoginManager
+
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -8,11 +14,39 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(login_id):
+    return users.query.get(int(login_id))
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE']=os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'sveta_barashkova_orm'
+    db_user = 'sveta_barashkova_orm'
+    db_password = '123'
+    host_ip = '127.0.0.1'
+    host_port = 5432
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "sveta_barashkova_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -22,6 +56,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.errorhandler(404)
 def not_found(err):
@@ -148,6 +183,9 @@ def start():
                 </li>
                 <li>
                     <a href="/lab8">Лабораторная работа 8</a>
+                </li>
+                <li>
+                    <a href="/lab9">Лабораторная работа 9</a>
                 </li>
 
             </ol>
