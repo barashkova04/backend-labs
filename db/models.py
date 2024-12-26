f4668926c659edc228d17006ec0407fcbc57889d
Светлана Барашкова <svetabarash04@mail.ru>
2024-12-26 10:13:53 +0700

    правки

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 from . import db
+from flask_login import UserMixin
 
-class users(db.Model):
+class users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     login = db.Column(db.String(30), nullable = False, unique = True)
     password = db.Column(db.String(162), nullable = False)
@@ -13,3 +14,5 @@ class articles(db.Model):
     is_favorite = db.Column(db.Boolean)
     is_public = db.Column(db.Boolean)
     likes = db.Column(db.Integer)
+
+    user = db.relationship('users', backref='articles')
